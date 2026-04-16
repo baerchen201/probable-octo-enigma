bedd5942ce7661becebf0898a400a547aded9b64
baer1 <videocreator@outlook.de>
2026-04-16 19:20:02 +0200

    Add pre- and post build

---
+++ postbuild.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Add some resource file that the mod needs or smth
+zip "release.zip" -jMM "some-resource-file"
---
+++ prebuild.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# idk what this could be used for, maybe adjusting version numbers or smth
+echo "Hello, World!"
